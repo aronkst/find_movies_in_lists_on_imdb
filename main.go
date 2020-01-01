ab4082c1fa7218234d419fac5789b9f306a52fd9
Aron <aron.0504@hotmail.com>
2020-01-01 09:44:05 -0300

    get metascore from list

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	document.Find("div.lister-item.mode-detail").Each(func(_ int, s *goquery.Selection) {
 		title := getValueFromSiteSelection(s, "div.lister-item-content h3.lister-item-header a", "")
 		score := getValueFromSiteSelection(s, "div.ipl-rating-widget div.ipl-rating-star.small span.ipl-rating-star__rating", "")
-		fmt.Println(title, score)
+		metascore := getValueFromSiteSelection(s, "div.inline-block.ratings-metascore span.metascore.favorable", "")
+		fmt.Println(title, score, metascore)
 	})
 }
