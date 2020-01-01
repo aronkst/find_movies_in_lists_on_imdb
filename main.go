468803a6e78bfbf69cdf12e399d910db98dc501d
Aron <aron.0504@hotmail.com>
2020-01-01 09:28:01 -0300

    removed duplicate function

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
@@ -14,26 +10,4 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(document)
-}
-
-func loadSite(url string) (*goquery.Document, error) {
-	response, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	defer response.Body.Close()
-
-	if response.StatusCode != 200 {
-		message := fmt.Sprintf("Status code error: %d %s", response.StatusCode, response.Status)
-		err := errors.New(message)
-		return nil, err
-	}
-
-	document, err := goquery.NewDocumentFromReader(response.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return document, nil
 }
